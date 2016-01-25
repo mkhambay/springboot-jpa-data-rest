a1a7905b9055307616461c4c2f21904da16a16e6
Mandeep Khambay <mandeep.khambay@gmail.com>
2016-01-25 14:12:59 -0800

    Added paging and sorting REST services

--- mk-race-app/src/main/java/com/mk/race/app/service/impl/DriverServiceImpl.java
+++ mk-race-app/src/main/java/com/mk/race/app/service/impl/DriverServiceImpl.java
@@ -9,10 +9,14 @@ import io.swagger.annotations.ApiOperation;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -27,7 +31,7 @@ import com.mk.race.app.service.DriverService;
 @RestController
 @RequestMapping("/v1/driver")
 @Api(value = "Driver Management services")
-public class DriverServiceImpl implements DriverService {
+public class DriverServiceImpl {
 	
 	@Autowired
 	DriverRepository driverRepository;	
@@ -60,6 +64,27 @@ public class DriverServiceImpl implements DriverService {
 	@ApiOperation(value = "Get driver by last name", notes = "Get driver by last name")
 	public List<Driver> getDriverByLastName(@PathVariable String lastName) {
 		List<Driver> result = driverRepository.findByLastName(lastName);
+		return result;
+	}
+	
+	@ResponseStatus(value = HttpStatus.OK)
+	@RequestMapping(value ="/sorted", method = RequestMethod.GET)
+	@ApiOperation(value = "Get driver sorted", notes = "Get driver sorted")
+	public List<Driver> getDriverSorted(@RequestParam("sortOn") String sortField, @RequestParam("direction") String direction) {
+		Sort sort = new Sort(Sort.Direction.fromString(direction), sortField);
+		List<Driver> result = (List<Driver>) driverRepository.findAll(sort);
+		return result;
+	}
+	
+	@ResponseStatus(value = HttpStatus.OK)
+	@RequestMapping(value ="/lastName/{lastName}/paged", method = RequestMethod.GET)
+	@ApiOperation(value = "Get driver by last name paged", notes = "Get driver by last name paged")
+	public List<Driver> getDriverByLastNamePaged(@PathVariable String lastName, @RequestParam("sortOn") String sortField, 
+			@RequestParam("direction") String direction, @RequestParam("start") int start, @RequestParam("size") int size) {
+		Sort sort = new Sort(Sort.Direction.fromString(direction), sortField);
+		Pageable pageable = new PageRequest(start, size, sort);
+		@SuppressWarnings("unchecked")
+		List<Driver> result = (List<Driver>) driverRepository.findByLastName(lastName, pageable);
 		return result;
 	}
 	
